b5f06bcf81237b5449c11e583756376d83418682
Jiawei Tian <ucaptia@ucl.ac.uk>
2021-03-31 20:07:38 +0100

    write unit test for particle when there is constant acceleration

--- Testing/nbsimSolarSystemSimulationTest.cpp
+++ Testing/nbsimSolarSystemSimulationTest.cpp
@@ -14,8 +14,9 @@ TEST_CASE( "Test for acceleration", "[some group identifier]" ) {
     Eigen::Vector3d acceleration2(2.0,2.0,2.0);
 
 
-    SECTION("Check no acceleration"){
+    SECTION("Check particle movement when there is no acceleration"){
 
+        // create an instance of Particle class
         nbsim::Particle particle(position,velocity);
 
         particle.integrateTimeStep(acceleration1, 5.0);
@@ -29,6 +30,21 @@ TEST_CASE( "Test for acceleration", "[some group identifier]" ) {
 
     }
     
+    SECTION("Check particle movement when there is constant acceleration"){
+
+        // create an instance of Particle class
+        nbsim::Particle particle(position,velocity);
+
+        particle.integrateTimeStep(acceleration2, 5.0);
+
+        // position and velocity after timestep
+        Eigen::Vector3d pos_2(26.0,30.0,25.0);
+        Eigen::Vector3d vel_2(10.0,11.0,10.0);
+
+        REQUIRE(particle.getPostion().isApprox(pos_2));
+        REQUIRE(particle.getVelocity().isApprox(vel_2));
+
+    }
 
 
 
